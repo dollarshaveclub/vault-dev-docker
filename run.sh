ea9bc710caf9d2e187b7c4f93bc4bcbfe4a53cb0
Alex Janckila <alex.janckila@dollarshaveclub.com>
2020-12-02 16:16:29 -0800

    update run.sh for k8s auth

--- run.sh
+++ run.sh
@@ -80,12 +80,12 @@ fi
 # Enable K8s auth (will only work when running in k8s)
 if [ -n "$VAULT_USE_K8S" ]; then
   cacert=${VAULT_CA_CERT:-"@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"}
-  k8stoken=${VAULT_K8S_TOKEN:-"@/var/run/secrets/kubernetes.io/serviceaccount/token"}
-  k8shost=${VAULT_K8S_HOST:-"https://kubernetes.default"}
+  k8stoken="$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+  k8shost=${VAULT_K8S_HOST:-"https://kubernetes:443"}
   vault auth enable kubernetes
   vault write auth/kubernetes/config \
     kubernetes_host=${k8shost} \
-    token_reviewer_jwt=${k8stoken} \
+    token_reviewer_jwt="${k8stoken}" \
     kubernetes_ca_cert="${cacert}"
   if [[ -f "$VAULT_K8SROLES_FILE" ]]; then
   	for k8srole in $(jq -rc '.[]' < "$VAULT_K8SROLES_FILE"); do
@@ -98,7 +98,7 @@ if [ -n "$VAULT_USE_K8S" ]; then
         bound_service_account_names=${serviceaccounts} \
         bound_service_account_namespaces=${namespaces} \
         policies=${policies} \
-        ttl=1h
+        ttl=24h
   	done
   else
     echo "$VAULT_K8SROLES_FILE not found, skipping"
